fbf3746364b5d5138ee6f0194a9105d69a1cdff9
beautythieves <96109098+beautythieves@users.noreply.github.com>
2023-02-09 12:18:03 +0100

    routes deployed

--- src/index.js
+++ src/index.js
@@ -18,10 +18,10 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-       <Route path= "/about" element={<About />}></Route>
-       <Route path= "/accomodation" element={<Accomodation />}></Route>
-       {/* if the user types in a wrong path, it will show the error page */}
-       <Route path= "*" element={<Error />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/accomodation" element={<Accomodation />}></Route>
+        {/* if the user types in a wrong path (none of the path below), it will show the error page */}
+        <Route path="*" element={<Error />}></Route>
       </Routes>
     </BrowserRouter>
     <Footer />
