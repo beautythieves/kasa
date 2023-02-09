b9215201e4fab56660854418dee0be2ec7979214
beautythieves <96109098+beautythieves@users.noreply.github.com>
2023-02-09 14:28:49 +0100

    test routeur ok

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+/* create home page */
+import React from 'react';
+/* import subHeader component */
+import SubHeader from '../components/subHeader/SubHeader';
+/* home page */
+export default  function About () {
+  return (
+    <div>
+      <SubHeader />
+      ergqeyqyhsrjhsr-
+    </div>
+  );
+}
