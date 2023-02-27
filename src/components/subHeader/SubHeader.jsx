91cd2da2193d2d934409ca6cf814bc484f1d40b8
beautythieves <96109098+beautythieves@users.noreply.github.com>
2023-02-27 11:28:42 +0100

    Updates

--- src/components/subHeader/SubHeader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./SubHeader.css";
-
-export default function SubHeader() {
-  return (
-      <p className="subHeader-text"> Chez vous, partout et ailleurs </p>
-  );
-}
--- src/pages/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-/* import error component*/
-import Error from '../components/Error/Error';
-
-export default  function ShowError () {
-  return (
-    <Error />
-  );
-}
